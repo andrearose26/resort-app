4cf2f7b408dfdbf5d93b3a40b83ee35b4433c878
andrearose26 <andreaa.lacson@gmail.com>
2020-06-22 16:34:11 -0400

    single room added

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-const SingleRoom = () => {
-    return (
-        <div>
-            Single Room
-        </div>
-    )
+import defaultBcg from '../images/room-1.jpeg';
+
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+
+import { Link } from 'react-router-dom';
+import { RoomContext } from '../context'
+
+export default class SingleRoom extends Component {
+    constructor(props) {
+        super(props)
+
+        console.log(this.props)
+    }
+
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <div>
+                Single Room
+            </div>
+        )
+    }
 }
 
-export default SingleRoom
